181f8e68b588c5237752d8fc04732f5cf9163702
Jesper Plantener <jep@edora.dk>
2016-02-16 09:53:20 +0100

    partly implemented component router

--- angular2-tour-of-heroes/app/app.component.ts
+++ angular2-tour-of-heroes/app/app.component.ts
@@ -1,92 +1,40 @@
 import {Component} from 'angular2/core';
-import {OnInit} from 'angular2/core';
-import {Hero} from './hero';
-import {HeroDetailComponent} from './hero-detail.component';
 import {HeroService} from './hero.service';
+import {HeroesComponent} from './heroes.component';
+import {DashboardComponent} from './dashboard.component';
+import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from 'angular2/router';
+
+@RouteConfig([
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: DashboardComponent,
+    useAsDefault: true
+  },
+  {
+    path: '/heroes',
+    name: 'Heroes',
+    component: HeroesComponent
+  }
+])
 
 @Component({
   selector: 'my-app',
   template: `
-  <h1>{{title}}</h1>
-  <h2>My Heroes</h2>
-  <ul class="heroes">
-    <li *ngFor="#hero of heroes" (click)="onSelect(hero)" [class.selected]="hero === selectedHero">
-      <span class="badge">{{hero.id}}</span> {{hero.name}}
-    </li>
-  </ul>
-  <my-hero-detail [hero]="selectedHero"></my-hero-detail>
+    <h1>{{title}}</h1>
+    <nav>
+    <a [routerLink]="['Dashboard']">Dashboard</a>
+    <a [routerLink]="['Heroes']">Heroes</a>
+    </nav>
+    <router-outlet></router-outlet>
   `,
-  styles:[`
-    .selected {
-      background-color: #CFD8DC !important;
-      color: white;
-    }
-    .heroes {
-      margin: 0 0 2em 0;
-      list-style-type: none;
-      padding: 0;
-      width: 10em;
-    }
-    .heroes li {
-      cursor: pointer;
-      position: relative;
-      left: 0;
-      background-color: #EEE;
-      margin: .5em;
-      padding: .3em 0em;
-      height: 1.6em;
-      border-radius: 4px;
-    }
-    .heroes li.selected:hover {
-      color: white;
-    }
-    .heroes li:hover {
-      color: #607D8B;
-      background-color: #EEE;
-      left: .1em;
-    }
-    .heroes .text {
-      position: relative;
-      top: -3px;
-    }
-    .heroes .badge {
-      display: inline-block;
-      font-size: small;
-      color: white;
-      padding: 0.8em 0.7em 0em 0.7em;
-      background-color: #607D8B;
-      line-height: 1em;
-      position: relative;
-      left: -1px;
-      top: -4px;
-      height: 1.8em;
-      margin-right: .8em;
-      border-radius: 4px 0px 0px 4px;
-    }
-    `],
-    directives: [HeroDetailComponent],
-    providers: [HeroService]
-  })
+  directives: [ROUTER_DIRECTIVES],
+  providers: [
+    ROUTER_PROVIDERS,
+    HeroService
+  ]
+})
 
-
-
-  export class AppComponent implements OnInit {
-    ngOnInit(){
-      this.getHeroes();
-    }
-
-    constructor(private _heroService: HeroService){
-    }
-
-    public title = 'Tour of Heroes';
-    public selectedHero: Hero;
-    public heroes: Hero[];
-
-    onSelect(hero: Hero){
-      this.selectedHero = hero;
-    }
-
-    getHeroes(){
-      this._heroService.getHeroes().then(heroes => this.heroes = heroes);
-    }
-  }
+export class AppComponent{
+  title = 'Tour of Heroes';
+}
